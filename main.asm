6390eb1367c9bd9de2edd36eefed024436ed5996
Joel Cutler <joelcutler108@gmail.com>
2025-09-16 16:13:00 +0100

    add loop code

--- main.asm
+++ main.asm
@@ -12,15 +12,19 @@ _start:
     mov rbx, 0 ; data index
     
     ; Code
-    inc rbx            ; >
-    dec rbx            ; <
-    inc byte [d + rbx] ; +
-    dec byte [d + rbx] ; -
-    call read          ; ,
-    call print         ; .
-    ; todo             ; [
-    ; todo             ; ]
-
+    inc rbx                 ; >
+    dec rbx                 ; <
+    inc byte [d + rbx]      ; +
+    dec byte [d + rbx]      ; -
+    call read               ; ,
+    call print              ; .
+    loop_start_0:           ; [
+      cmp byte [d + rbx], 0
+      je loop_end_0
+      ; loop body
+      jmp loop_start_0
+    loop_end_0:             ; ]
+     
     ; Exit
     mov rax, 60       ; sys_exit
     mov rdi, 0        ; exit code 0
